5546f9265ec30d91bfb30033d5b66d3e1da17435
Gabriel Sargent <gabe2sargent@gmail.com>
2023-03-22 05:41:48 -0500

    loop

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const names = ['Guadalupe','Ollie', 'Aki'];
+
+function writeCards(names, event) {
+    let messages =[]
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+    }
+
+    return messages;
+  }
+
+  console.log(writeCards(names));
+
+
+
+function countDown(n) {
+    console.log(n);
+    return n? countDown(n -1) :0;
+  }
+  countDown(10);
+
+
+
